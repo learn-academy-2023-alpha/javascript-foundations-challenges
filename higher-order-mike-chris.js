ef13d9acad0ead640635d2403e7152b19ea504a2
mikerogers04 <mikerogers04@hotmail.com>
2023-02-22 12:01:00 -0800

    Added flexible functions for filtering unique values

--- higher-order-mike-chris.js
+++ higher-order-mike-chris.js
@@ -7,7 +7,7 @@ const multTen = (array) => {
     return array.map(value => value * 10)
 }
 //log function and pass in test array
-console.log(multTen(arr1))
+//console.log(multTen(arr1))
 
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
 const arr2 = [2, 7, 3, 5, 8, 10, 13]
@@ -19,7 +19,7 @@ const onlyOdd = (array) => {
     return array.filter(value => value % 2 === 1)
 }
 //log function and pass in test array
-console.log(onlyOdd(arr2))
+//console.log(onlyOdd(arr2))
 
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use the typeof operator.
 const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
@@ -31,19 +31,27 @@ const onlyLetters = (array) => {
     return array.filter(value => typeof value === "string").join("")
 }
 //log function and pass in test array
-console.log(onlyLetters(comboArr))
+//console.log(onlyLetters(comboArr))
 
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
 const filterArrayValues = [58, "", "abcd", true, null, false, 0]
 // output: [58, "abcd", true]
 
 //create a function that takes in an array
-const filteredArray = (array) => {
-    //filter an array and returns values that are not false, zero, null, or blank values
-    return array.filter(value => value !== false && value !== null && value != 0 && value !== "")
+// const filteredArray = (array) => {
+//     //filter an array and returns values that are not false, zero, null, or blank values
+//     return array.filter(value => value !== false && value !== null && value != 0 && value !== "")
+// }
+// //log function and pass in test array
+// console.log(filteredArray(filterArrayValues))
+
+//create a function and pass in two arrays
+const flexibleFilter = (array1, array2) => {
+    //return an array of values from the first array that are not found in the second array
+    return array1.filter(value => array2.indexOf(value)=== -1)
 }
-//log function and pass in test array
-console.log(filteredArray(filterArrayValues))
+//log function and pass in test array and test array with false, 0, null, ""
+console.log(flexibleFilter(filterArrayValues, [false, null, 0, ""]))
 
 // 🏔 Stretch Goals
 // Create a function that takes in a string and returns a new string with all the vowels removed.
@@ -56,7 +64,15 @@ const noVowels = (string) => {
     return string.split("").filter(value => value !== "a" && value !== "e" && value !== "i" && value !== "o" && value !== "u" && value !== "y").join("")
 }
 //log function and pass in test string 
-console.log(noVowels(str))
+//console.log(noVowels(str))
+
+//create function and pass in string and array
+const flexbileStringFilter = (string, array) => {
+    //split a string into an array of characters, filter the new array to return values that are not in the paramter array, and join array back into a string 
+    return string.split("").filter(value => array.indexOf(value) === -1).join("")
+}
+//log function and pass in test string and array of parameters
+console.log(flexbileStringFilter(str, ["a", "e", "i", "o", "u"]))
 
 // Create a function that takes in two arrays as arguments returns one array with no duplicate values.
 const arr3 = [3, 7, 10, 5, 4, 3]
@@ -80,4 +96,4 @@ const easyUniques = (array1, array2) => {
     return [...new Set(array1.concat(array2))]
 }
 //log function and pass in two test arrays
-console.log(easyUniques(arr3, arr4))
+//console.log(easyUniques(arr3, arr4))
