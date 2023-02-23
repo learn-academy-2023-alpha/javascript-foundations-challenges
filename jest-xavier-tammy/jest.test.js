0d9f5080b9cd3cc6c9b87eb969e14ebdaff1abae
tammytran96 <tammytran96@gmail.com>
2023-02-23 14:39:51 -0800

    working through, switching roles

--- jest-xavier-tammy/jest.test.js
+++ jest-xavier-tammy/jest.test.js
@@ -117,12 +117,94 @@ else {
 // Create the function that will make the test pass.
 // Write the test for a function that takes in a fruit and returns "yellow" if the argument is banana, "red" if apple and "purple" if grape.
 // Create the function that will make the test pass.
+
+//Pseudocode:
+// Create a function that takes a fruit (strings) and returns a color (string)
+// input - fruit (string), banana, apple, or grape
+// output - color (string) - yellow, red, or purple
+// Create a function named fruitColor
+
+describe("fruitColor", () => {
+    it("returns a color when user inputs in a fruit", () => {
+      expect(fruitColor("apple")).toEqual("red")
+    })
+  })
+
+  let ourFruit = "banana"
+  const fruitColor = (fruit) => {
+    if(fruit == "banana") {
+        return "yellow"
+    } else if(fruit == "apple"){
+        return "red"
+    } else if(fruit == "grape"){
+        return "purple"
+    } else {
+        return "please enter desired fruit"
+    }
+  }
+
+
 // Write the test for a function called rick that returns "Morty".
 // Create the function that will make the test pass.
+// Pseudocode: 
+// Create a function called rick
+// Returns a string "Morty"
+// input - none?
+
+describe("rick", () => {
+    it("returns a string called 'Morty'", () => {
+      expect(rick()).toEqual("Morty")
+    })
+  })
+
+const rick = () => {
+    return "Morty"
+}
+
 // Write the test for a function called greeter that takes a name as an argument and returns a greeting with that name to the screen.
 // Create the function that will make the test pass.
+//Pseudocode:
+// Create a function named greeter
+// input - a name (string)
+// output - a greeting with that name (string); output will be string interpolated
+
+describe("greeter", () => {
+    it("returns a greeting with a name", () => {
+      expect(greeter("Xavier")).toEqual("Hello, Xavier!")
+    })
+  })
+
+  let myName = "Xavier"
+  const greeter = (name) => {
+return `Hello, ${name}!`
+  }
+
+
 // Write the test for a function called oddOrEven that takes a number as an argument and logs whether the number is odd or even.
 // Create the function that will make the test pass.
+// Pseudocode
+// Create a function called oddOrEven
+// input - a number as an argument
+// output - will log whether the number is odd or even
+
+describe("oddOrEven", () => {
+    it("returns whether the number is odd or even", () => {
+      expect(oddOrEven(43)).toEqual("odd")
+    })
+  })
+
+let myNumber = 49
+const oddOrEven = (number1) => {
+if(number1 % 2 === 0){
+    return "even"
+} else if(number1 % 2 !== 0){
+    return "odd"
+} else {
+    return "please enter a numerical value"
+}
+}
+
+
 // Write the test for a function called doubler that takes a number and returns the result of the number multiplied by 2.
 // Create the function that will make the test pass.
 // Write the test for a function called multiply that takes two numbers as arguments and logs the result of one of the numbers multiplied by the other.
