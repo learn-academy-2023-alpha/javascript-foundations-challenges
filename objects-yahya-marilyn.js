17dcffd3eef338f7f3d61e95323e55e92a4b54fe
Marilyn Remigio <marilynremigio@gmail.com>
2023-02-24 11:56:17 -0800

    completed more object challenge quesitons

--- objects-yahya-marilyn.js
+++ objects-yahya-marilyn.js
@@ -20,8 +20,15 @@ const person = {
 // console.log(person.homePlanet)
 // console.log(person.personInfo())
 
+// const describeProduct = function (product) {
+//   return `The product is a ${product.name}. It costs $${product.price}`
+// } 
 
-  
+// totalWithTax: function () {
+//   let salesTax = this.price * .07 
+//   let totalPrice = this.price + salesTax
+//   return totalPrice.toFixed(2)
+// }
   const product = {
     name: "chair",
     price: 24.99,
@@ -36,43 +43,129 @@ const person = {
   }
 //   Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
 //   Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
-//   Consider this variable:
+// 
+
 // Pseudo: create a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
 // Pseudo: create a function called totalWithTax that takes the product object as an argument and logs the total price of the chair that includes a 7% sales tax rounded to two decimals.
+// console.log(describeProduct(product))
+// console.log(totalWithTax(product))
+
 // console.log(product.describeProduct())
 // console.log(product.totalWithTax())
 
   
-  const lunch = {
+// const pbSandwich = function (lunch) {
+//   return `The ingredients for a ${lunch.name}, ${lunch.type} are ${lunch.ingredients}.`
+// }  
+const lunch = {
     name: "PB and Banana",
     type: "sandwich",
-    ingredients: ["bread", "peanut butter", "banana"]
+    ingredients: ["bread", "peanut butter", "banana"],
+    pbSandwich: function () {
+      return `The ingredients for a ${this.name} ${this.type} are ${this.ingredients}.`
+    }
   }
 //   Write the code that accesses the ingredients property.
+// Pseudo: console.log the object named lunch and use . notation to access key: ingredients
+// console.log(lunch.ingredients) ->[ 'bread', 'peanut butter', 'banana' ]
+
 //   Write the code that access the third ingredient of the lunch object.
+// Pseudo: console.log the object named lunch and use . notation to access key: ingredients and find the index of third item [2]
+// console.log(lunch.ingredients[2]) -> banana
+
 //   Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+// Pseudo: create a function called pbSandwich that takes the lunch object as an argument and uses string interpolation to return "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+//console.log(pbSandwich(lunch))
+
 //   Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+// console.log(lunch.pbSandwich()) -> The ingredients for a PB and Banana sandwich are bread,peanut butter,banana.
+
+
+
+
 //   Consider this variable:
   
-  const animals = [
+  // const animals = [
+  //   { name: "Waffles", type: "dog", age: 7 },
+  //   { name: "Fluffy", type: "cat", age: 14 },
+  //   { name: "Spelunky", type: "dog", age: 4 },
+  //   { name: "Hank", type: "cat", age: 11 }
+  // ]
+//   Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+
+// Pseudo: create function named cats that takes in the animal object as parameter and use .filter() to return a new array with only objects that are cats
+
+// const cats = (object) => {
+//   return object.filter(value => {
+//      return value.type === "cat"
+//   })
+// }
+// console.log(cats(animals))
+
+//   Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+
+// const cats = (object) => {
+//   return object.map(value => {
+//      return value.name
+//   })
+// }
+// console.log(cats(animals))
+
+//   Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+
+// const cats = (object) => {
+//     return object.filter(value => {
+//        return value.age > 10 
+//     }).map(value => {
+//       return value.name
+//     })
+//   }
+//   console.log(cats(animals))
+
+//   Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+const animals = [
     { name: "Waffles", type: "dog", age: 7 },
     { name: "Fluffy", type: "cat", age: 14 },
     { name: "Spelunky", type: "dog", age: 4 },
     { name: "Hank", type: "cat", age: 11 }
   ]
-//   Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
-//   Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
-//   Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
-//   Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+
+// Pseudo: create function named aboutAnimal that takes in an array of animal objects and returns a new array with string interpolated sentences about each animal 
+
+const aboutAnimal = (object) => {
+  return object.map(value => {
+    return `${value.name} is a ${value.type} that is ${value.age} years old.`
+  })
+}
+console.log(aboutAnimal(animals))
+
+
 //   Consider this variable:
   
-  const author = {
-    name: "H. G. Wells",
+  // const author = {
+  //   name: "H. G. Wells",
+  //   genre: "science fiction"
+  // }
+
+
+//   Write the code that destructures the author object so that the following code snippet will run successfully:
+
+// cannot deconstruct current object because it doesn't have a nested object contained inside
+//console.log(`${author.name} is a ${author.genre} author`)  // output: "H. G. Wells is a science fiction author"
+
+const author = {
+  about: {
+    name: "H. G. Wells"
+  },
+  favorite: {
     genre: "science fiction"
   }
-//   Write the code that destructures the author object so that the following code snippet will run successfully:
-//   console.log(`${name} is a ${genre} author`)
-//   // output: "H. G. Wells is a science fiction author"
+}
+let {name} = author.about
+let {genre} = author.favorite
+console.log(`${name} is a ${genre} author`) // output: "H. G. Wells is a science fiction author"
+
 //   Consider this variable:
   
   const pokeOne = {
